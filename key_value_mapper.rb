0ce51d3f67d8b86ea3e865efd17024bb671a8cc8
renoke <renaud.kern@gmail.com>
2009-07-31 15:34:23 +0200

    add key_value_mapper.rb

---
+++ key_value_mapper.rb
@@ -0,0 +1,16 @@
+
+
+dir = File.expand_path(File.dirname(__FILE__)) + '/lib'
+require dir + '/adapters/adapter'
+require dir + '/extensions/hash'
+require dir + '/extensions/array'
+require dir + '/config'
+require dir + '/core_mash'
+require dir + '/crud'
+require dir + '/validatable/validatable'
+
+require dir + '/document'
+
+
+
+
